c0ca5c24cf83e41a5ec8c20f98f012576cb773a6
🔥 TommY ❄️ <lihroff@icloud.com>
2019-06-17 11:05:01 +0800

    [chore]: format

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Comment item is the default configuration
+module.exports = {
+  printWidth: 100,
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: true,
+  singleQuote: true,
+  // jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  // Don't specify parser to make sure muti-language formatting.
+  // parser: "typescript",
+};
